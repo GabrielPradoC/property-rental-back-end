a4e6de60c20adb10a78cde2993836938a0f96e4b
Gabriel Prado <69232929+GabrielPradoC@users.noreply.github.com>
2024-04-29 13:42:22 -0300

    build: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+// Libs
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+// Modules
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Property Rental API')
+    .setDescription('The Property Rental API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
