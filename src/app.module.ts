f8383dac52704fbf33d6fad91c95b9288d189735
Gabriel Prado <69232929+GabrielPradoC@users.noreply.github.com>
2024-04-29 14:31:53 -0300

    build: Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 // Libs
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Modules
 import { HealthcheckModule } from './modules/healthcheck/healthcheck.module';
 
 @Module({
-  imports: [HealthcheckModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_URL,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: false,
+    }),
+    HealthcheckModule,
+  ],
 })
 export class AppModule {}
